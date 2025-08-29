4034efc147c7dc80863e36ccce32a1ed715b7180
Zwe Let Yar <letyarzwe@gmail.com>
2025-08-29 21:43:48 +0630

    Account Model Schema

---
+++ database/account.model.ts
@@ -0,0 +1,43 @@
+import { model, Schema, Types } from "mongoose";
+
+interface IAccount {
+  userId: Types.ObjectId;
+  name: string;
+  image?: string;
+  password?: string;
+  provider: string;
+  providerAccountId: string;
+}
+
+const AccountSchema = new Schema(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      require: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+    },
+    password: {
+      type: String,
+    },
+    provider: {
+      type: String,
+      required: true,
+    },
+    providerAccountId: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Account = model<IAccount>("Account", AccountSchema);
+
+export default Account;
--- database/user.model.ts
+++ database/user.model.ts
@@ -11,38 +11,43 @@ interface Iuser {
   reputation?: number;
 }
 
-const UserSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
+const UserSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    bio: {
+      type: String,
+    },
+    location: {
+      type: String,
+    },
+    portfolio: {
+      type: String,
+    },
+    reputation: {
+      type: Number,
+      default: 0,
+    },
   },
-  username: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  image: {
-    type: String,
-    required: true,
-  },
-  bio: {
-    type: String,
-  },
-  location: {
-    type: String,
-  },
-  portfolio: {
-    type: String,
-  },
-  reputation: {
-    type: Number,
-    default: 0,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const User = model<Iuser>("User", UserSchema);
 
