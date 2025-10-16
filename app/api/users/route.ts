f7bd531619e0c3ec71a590b254f2fe9f22344139
Zwe Let Yar <letyarzwe@gmail.com>
2025-10-16 18:52:16 +0630

    refactor response update

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -4,7 +4,7 @@ import { NextResponse } from "next/server";
 
 //define function
 const handleSuccessResponse = (data: unknown, status: number = 200) => {
-  const total = Array.isArray(data) ? data.length : 1;
+  const total = Array.isArray(data) ? data.length : 1; //coz data.length can't be used in metadata
   return NextResponse.json(
     {
       meta: {
