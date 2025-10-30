c1bed5304e2e4e904d92c75d5eba6cab2f989ff1
Zwe Let Yar <letyarzwe@gmail.com>
2025-10-30 20:31:25 +0630

    RegisterWithCredentials Server Action

---
+++ lib/actions/SignUpWithCredentials.action.ts
@@ -0,0 +1,68 @@
+"use server";
+
+import mongoose from "mongoose";
+import dbConnect from "../dbConnect";
+import { handleErrorResponse, handleSuccessResponse } from "../response";
+import validateBody from "../vaildateBody";
+import SignupSchema from "../schemas/SignupSchema";
+import User from "@/database/user.model";
+import Account from "@/database/account.model";
+import bcrypt from "bcryptjs";
+import { signIn } from "@/auth";
+
+export async function SignUpWithCredentials(params: {
+  name: string;
+  username: string;
+  email: string;
+  password: string;
+}) {
+  await dbConnect();
+  const session = await mongoose.startSession();
+  session.startTransaction();
+
+  try {
+    const validatedData = validateBody(SignupSchema, params);
+    //@ts-expect-error
+    const { name, username, email, password } = validatedData;
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      throw new Error("Email already exists!");
+    }
+
+    const existingUsername = await User.findOne({ username });
+    if (existingUsername) {
+      throw new Error("Username already exists");
+    }
+
+    const [newUser] = await User.create(
+      [
+        {
+          name,
+          username,
+          email,
+        },
+      ],
+      { session }
+    );
+
+    await Account.create([
+      {
+        userId: newUser._id,
+        name,
+        password: await bcrypt.hash(password, 10),
+        provider: "credentials",
+        providerAccountId: email,
+      },
+    ]);
+
+    await session.commitTransaction();
+    await signIn("credentials", { email, password, redirect: false });
+    return handleSuccessResponse(newUser);
+  } catch (e) {
+    await session.abortTransaction();
+    return handleErrorResponse(e);
+  } finally {
+    await session.endSession();
+  }
+}
--- lib/schemas/UserSchema.ts
+++ lib/schemas/UserSchema.ts
@@ -15,7 +15,7 @@ import { z } from "zod";
 
 const UserSchema = z.object({
   name: z.string().min(1, "Name is required"),
-  username: z.string().min(6, "Username must be at least 6 characters"),
+  username: z.string().min(3, "Username must be at least 3 characters"),
   email: z.string().email("Invalid email address"),
   image: z.string().url("Invalid image URL"),
 });
